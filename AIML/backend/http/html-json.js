3c7b73108c8ab7ff18c08b0efc702e713147be40
dhruvv06 <dhruv.bhadauria6104@gmail.com>
2025-02-14 10:33:07 +0530

    new updated

--- AIML/backend/http/html-json.js
+++ AIML/backend/http/html-json.js
@@ -1,17 +1,51 @@
 const http = require('http');
-const fs = require('fs/promises')
+const fs = require('fs/promises');
+const server = http.createServer(async (req,res) =>{
 
-const server= http.createServer(async (req,res) =>
-{
-    //res.statuscode =200;
-    //res.setheader('content-type','text/plain');
-    res.writeHead(200,{'content-type':'text/plain'});
-    // res.end('Hello,world');
+    if(req.url === '/'){
+        res.writeHead(200,{
+            'content-type': 'application/json'
+        })
+        const data = await fs.readFile('./aa.json','utf-8')
+        res.end(JSON.stringify(JSON.parse(data)))
+    }
+    else if(req.url === '/home'){
+        res.writeHead(200,{
+            'content-type':'text/html'
+        })
+        const data = await fs.readFile('./codepen.html','utf-8');
+        res.end(data)
+    }
+    else{
+        res.writeHead(200,{
+            'content-type': 'text/html'
+        })
+        const data = await fs.readFile('./error.html','utf-8')
+        res.end(data)
+    }
 
-    const d = await fs.readFile("./aa.json","utf-8")
-    res.end(d)
+
+
+
+
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type','text/json');
+    // res.writeHead(200,{'Content-Type': 'text/plain'});    this is only enough for previous 2 lines
+
+    
+    // const data = user.map(u => {
+    //     return u.name;
+    // });
+
+    // let data = ""
+    // user.forEach((u) => {
+    //     data += u.name + ' ';
+    // })
+    // res.end(JSON.stringify(data));
+    // const data = await fs.readFile("./aa.json",'utf-8');
+    //  res.end(data);
 });
-port = 3000;
-server.listen(3000 ,()=> {
-    console.log(`server is running on ${port}`);
-})
+port = 3001;
+server.listen(port, () => {
+    console.log(`server is runing on ${port}`);
+});
